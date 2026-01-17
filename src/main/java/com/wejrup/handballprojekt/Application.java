0200104358f7d5cf5f4863f47f2e5a622671b75a
Victor Wejrup <wejrup@gmail.com>
2026-01-17 10:29:40 +0100

    Commit

--- src/main/java/com/wejrup/handballprojekt/Application.java
+++ src/main/java/com/wejrup/handballprojekt/Application.java
@@ -3,6 +3,7 @@ package com.wejrup.handballprojekt;
 import com.wejrup.handballprojekt.controller.data.Database;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,6 +19,11 @@ public class Application extends javafx.application.Application {
 
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("scenes/Menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 800);
+
+        // Set the application icon
+        Image icon = new Image(getClass().getResourceAsStream("/com/wejrup/handballprojekt/Icons/handballAppIcon.png"));
+        stage.getIcons().add(icon);
+
         stage.setTitle("Handball Match System");
         stage.setScene(scene);
         stage.show();
